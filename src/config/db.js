05d379938edf89f44d254fbbf9ffaa09f9c0fadd
Wesle Leal <62607761+WesleDev@users.noreply.github.com>
2020-06-23 11:58:01 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg')
 
 module.exports = new Pool({
-    user: 'postgres', //Seu usuário do PostgreSQL aqui
-    password: '335923', //Sua senha do PostgreSQL aqui
+    user: '', //Seu usuário do PostgreSQL aqui
+    password: '', //Sua senha do PostgreSQL aqui
     host: 'localhost',
     port: 5432,
     database: 'foodfy'
